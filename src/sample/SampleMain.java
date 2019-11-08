4ffad6a902ea56e347451bdb0f19b54160c9f5d0
n-hashimoto-01 <n-hashimoto@tsrweb.co.jp>
2019-11-08 11:20:38 +0900

    JavaDocを整備

--- src/sample/SampleMain.java
+++ src/sample/SampleMain.java
@@ -1,12 +1,12 @@
 package sample;
 
 /**
- * @author 
+ * サンプルクラス
+ * @author n-hashimoto-01 
  *
  */
 public class SampleMain {
 	public static void main(String[] args) {
 		System.out.println("end");
 	}
-
 }
